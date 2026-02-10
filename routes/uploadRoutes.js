8c6d7a8b9f8b7379c14a68c683d4c1b80220d5f6
skoko <skokomi396@gmail.com>
2026-02-11 02:33:00 +0500

    You will know later

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -5,8 +5,6 @@ const uploadController = require('../controllers/uploadController');
 const { protect } = require('../middleware/auth');
 
 const upload = multer({ storage: multer.memoryStorage() });
-
-// Endpoint: POST /api/upload
 router.post('/', protect, upload.single('file'), uploadController.uploadFile);
 
 module.exports = router;
